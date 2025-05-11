c20cfb5857efa6f10594e4aaa3f5f5d8becf6abe
Aye Chan Aung Thwin <ayechanaungthwin@gmail.com>
2025-05-11 20:55:58 +0700

    styles

--- WpfLocalizationTest/WpfLocalizationTest/SplashScreen.xaml.cs
+++ WpfLocalizationTest/WpfLocalizationTest/SplashScreen.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace WpfLocalizationTest
@@ -19,6 +20,24 @@ namespace WpfLocalizationTest
                     loadLanguage(WpfLocalizationTest.Language.Myanmar);
                     break;
             }
+
+            Task.Run(() =>
+            {
+                for (var i = 2; i >= 0; i--)
+                {
+                    Thread.Sleep(1000);
+                    Dispatcher.Invoke(() =>
+                    {
+                        ShowText.Text = i.ToString();
+                    });
+                }
+
+                Dispatcher.Invoke(() =>
+                {
+                    new MainWindow().Show();
+                    Close();
+                });
+            });
         }
         
         private void loadLanguage(Language language)
@@ -27,13 +46,25 @@ namespace WpfLocalizationTest
             Application.Current.Resources.MergedDictionaries.Clear();
 
             // Add your application-wide resources if needed
-            Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri($"pack://application:,,,/WpfLocalizationTest;component/Resources/{language.ToString()}.xaml") });
+            addToResources(new Uri($"pack://application:,,,/Resources/{language.ToString()}.xaml"));
+            
+            loadStyle();
         }
 
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        public void addToResources(Uri uri)
+        {
+            Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary { Source = uri });
+        }
+
+        private void loadStyle()
+        {
+            addToResources(new Uri($"pack://application:,,,/Styles/Style.xaml"));
+        }
+
+        /*private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
             new MainWindow().Show();
             Close();
-        }
+        }*/
     }
 }
